18c87658ccd161b79d32a779f136248ff7214d97
qianchenxu <2791685195@qq.com>
2026-05-06 10:04:40 +0800

    ai对话修

--- src/main/java/com/config/InterceptorConfig.java
+++ src/main/java/com/config/InterceptorConfig.java
@@ -18,7 +18,8 @@ public class InterceptorConfig extends WebMvcConfigurationSupport {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(getAuthorizationInterceptor()).addPathPatterns("/**").excludePathPatterns("/static/**");
+        registry.addInterceptor(getAuthorizationInterceptor()).addPathPatterns("/**").excludePathPatterns("/static/**",
+                "/admin/**", "/front/**", "/public/**");
         super.addInterceptors(registry);
     }
 
